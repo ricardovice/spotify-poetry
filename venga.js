ddc9781ffc1cd7c96a23a61deec8cfa13cde0af6
Ricardo Santos <ricardo@spotify.com>
2012-06-15 12:17:10 +0200

    VENGA BLIXT

--- venga.js
+++ venga.js
@@ -5,8 +5,10 @@ function vengaBuscarUno(vengaTitulo, cb) {
 
     var vengaBusca = encodeURIComponent('title:"' + vengaTitulo + '"');
     $.get(vengaSpotifyURL + vengaBusca, function(data) {
+        console.log(data)
         for (var i = 0; i < data.tracks.length; i++) {
             var track = data.tracks[i];
+            //console.log(track.name.toLowerCase() + '==' + vengaTitulo)
             if (track.name.toLowerCase() == vengaTitulo) {
                 cb(data.tracks[i]);
                 return;
@@ -18,10 +20,12 @@ function vengaBuscarUno(vengaTitulo, cb) {
 
 function vengaBuscar(vengaFrase, cb) {
     var vengaPalabras = vengaFrase.split(' ');
+    // TODO: join palabras
     vengaBuscarUno(vengaFrase, function(track) {
         if (track) {
             // TODO: Instead of calling callback here, add track to a list and look up remaining words
             //       (if all words have been looked up, THEN call the callback!)
+            console.log("HOLA TIO")
             cb([track]);
         } else {
             // TODO: Instead of failing here, try a new query with fewer words
@@ -35,15 +39,16 @@ $(function() {
         vengaBuscar($('#venga-frase').val(), function(tracks) {
             if (tracks) {
                 for (var i = 0; i < tracks.length; i++) {
-                    console.log(tracks[i].name);
+                    console.log(tracks[i].href.substring(14));
+                    vengaTracks.push(tracks[i].href.substring(14));
                 }
             } else {
-                alert(':´(');
+                alert(':(');
             }
         });
     });
 
-    var vengaTracks = ['2gqGPuOW5gUcpOm6Vt03S7', '0SY8oIKugUoToaPujEfghp'];
+    var vengaTracks = [] 
     var vengaIframe = $('#venga-musica');
     var vengaUrl = 'https://embed.spotify.com/?uri=spotify:trackset::' + vengaTracks.join(',');
     vengaIframe.attr('src', vengaUrl);
